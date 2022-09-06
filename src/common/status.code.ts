98b37d6f77e55644bc27070f7893d65786572332
Alexander <renyreny125tt@gmail.com>
2022-09-06 15:17:35 +0300

    feat: add status codes

---
+++ src/common/status.code.ts
@@ -0,0 +1,16 @@
+interface IStatusCode {
+  [key: string]: number;
+}
+
+const statusCode: IStatusCode = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export default statusCode;
